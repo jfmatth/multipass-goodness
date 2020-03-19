2403c126dbb2f0671badea7d9ac22d5426567787
John Matthew <john@compunique.com>
2020-03-19 12:24:22 -0700

    remove redundant scripts

--- std.cmd
+++
@@ -1,6 +0,0 @@
-if "%1"=="" GOTO END
-    multipass launch --name %1 --cpus=1 --mem=2g --disk=50gb
-    multipass exec %1 -- ssh-import-id gh:jfmatth
-    multipass exec %1 -- sudo apt-get update -y
-    multipass exec %1 -- sudo apt-get upgrade -y
-:end
