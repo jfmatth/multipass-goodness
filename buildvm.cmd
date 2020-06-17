17892bc022156e6dff44de45a0c87355fceeb649
John Matthew <jmatthew@red8.com>
2020-06-17 11:21:25 -0700

    improvements and new parameters for version of ubuntu

--- buildvm.cmd
+++ buildvm.cmd
@@ -1,7 +1,10 @@
 @echo off
 :: Given a name and type, we'll build a VM for the user
 
-:: params: %1 - Name, %2 - Type of VM
+:: params
+:: %1 - Name
+:: %2 - Type of VM
+:: %3 - image name to use (ubuntu version)
 
 :: fail if parameters are blank
 IF "%1"=="" GOTO help
@@ -12,21 +15,23 @@ GOTO %2
 
 :nano
     ECHO Building nano sized VM (1cpu x 1g x 25g) %1 / Importing keys / updating files, Stand by...
-    multipass launch --name %1 --cpus=1 --mem=1g --disk=25gb
+    multipass launch --name %1 --cpus=1 --mem=1g --disk=25gb %3
     GOTO cleanup
 :std
     ECHO Building standard sized VM (1cpu x 2g x 25g) %1 / Importing keys / updating files, Stand by...
-    multipass launch --name %1 --cpus=1 --mem=2g --disk=25gb
+    multipass launch --name %1 --cpus=1 --mem=2g --disk=25gb %3
     GOTO cleanup
 :big
     ECHO Building big sized VM (2cpu x 4g x 50g) %1 / Importing keys / updating files, Stand by...
-    multipass launch --name %1 --cpus=2 --mem=4g --disk=50gb
+    multipass launch --name %1 --cpus=2 --mem=4g --disk=50gb %3
     GOTO cleanup
 
 :cleanup
     multipass exec %1 -- ssh-import-id gh:jfmatth
     multipass exec %1 -- sudo apt-get update -y
     multipass exec %1 -- sudo apt-get upgrade -y
+    multipass exec %1 -- sudo apt autoremove -y
+    multipass exec %1 -- sudo reboot
 
 GOTO end
 
@@ -34,6 +39,7 @@ GOTO end
     ECHO.
     ECHO buildvm.cmd {name} {size}
     ECHO {name} - name of the VM
-    ECHO {size} - nano(1x1), std(1x2), big(2x4)
+    ECHO {size} - nano(1cpu x 1gb x 25gb), std(1cpu x 2gb x 25gb), big(2cpu x 4gb x 50gb)
+    ECHO {version} - Version of Ubuntu to use from "mp find"
     ECHO.
 :end
